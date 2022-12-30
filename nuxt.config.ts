7b9ce40c2040dba422ad8590b8368fb797275f8c
tatseo <truoff21@gmail.com>
2022-12-30 20:54:42 +0100

    init commit

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss'],
+  runtimeConfig: {
+    mongoDB_URI: process.env.MONGO_DB_URI
+  }
+})
